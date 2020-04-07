01ae43ccecb193578e0dd2920e975c274440627e
satyam0460 <satyam0460@gmail.com>
2020-04-07 17:32:45 +0530

    Student Attendance

---
+++ src/main/java/inc/satyam/attendancemanager/attendance.java
@@ -0,0 +1,18 @@
+package inc.satyam.attendancemanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class attendance extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.attendance,container,false);
+    }
+}
